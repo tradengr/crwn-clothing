23e435d8c0688d7064abc19f1644c610cba64c9f
Nicolo Valbuena <nicolo.valbuena@gmail.com>
2023-04-26 18:04:00 +0800

    replaced useState with reducers

--- client/src/components/cartIcon/CartIcon.component.jsx
+++ client/src/components/cartIcon/CartIcon.component.jsx
@@ -4,11 +4,10 @@ import './CartIcon.styles.scss';
 import { CartContext } from '../../contexts/cart.context';
 
 export default function CartIcon() {
-  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
-  const handleCartToggle = () => setIsCartOpen(!isCartOpen);
+  const { toggleCart, cartCount } = useContext(CartContext);
 
   return (
-    <div className='cart-icon-container' onClick={handleCartToggle}>
+    <div className='cart-icon-container' onClick={toggleCart}>
       <Icon className='shopping-icon'/>
       <span className='item-count'>{cartCount}</span>
     </div>
--- client/src/routes/navbar/Navbar.component.jsx
+++ client/src/routes/navbar/Navbar.component.jsx
@@ -15,8 +15,6 @@ export default function Navbar() {
   const { currentUser } = useContext(UserContext);
   const { isCartOpen } = useContext(CartContext);
 
-  console.log(currentUser)
-
   return (
     <>
       <div className="navigation">
