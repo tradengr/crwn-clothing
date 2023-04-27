7ed56dab10de7ed7c3b004c8e15df4e87344c1fa
Nicolo Valbuena <nicolo.valbuena@gmail.com>
2023-04-27 21:19:34 +0800

    added dynamic imports on router components

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,4 +1,3 @@
-// import { compose, createStore, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -16,15 +15,6 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean); 
 
-// const composeEnhancer = (
-//   process.env.NODE_ENV === 'development' 
-//   && window 
-//   && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-// ) || compose;
-
-// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
-
-// export const store = createStore(persistedReducer, undefined, composedEnhancers);
 export const store = configureStore({ 
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
